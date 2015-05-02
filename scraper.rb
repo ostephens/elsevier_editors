e738ff4e733cb31119428f4600e009b9044ee40e
Owen Stephens <owen@ostephens.com>
2015-05-02 23:01:12 +0100

    Added initial scraper

--- scraper.rb
+++ scraper.rb
@@ -1,24 +1,70 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'nokogiri'
+require 'uri'
+require 'cgi'
+require 'open-uri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+class ElsevierJournal
+    def initialize(title,kb_uri)
+        @kb_uri = kb_uri
+        @editorial_board = []
+    end
+    
+    attr_reader :title, :kb_uri
+    attr_accessor :about_uri, :editorial_uri, :editorial_board
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries. You can use whatever gems are installed
-# on morph.io for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+    def getAboutUri
+      # Retrieve page from kb_uri
+      # Find link with text 'About this journal'
+      # store link as about_uri
+      about = Nokogiri::XML(open(@kb_uri))
+      @about_uri = about.xpath('//a[contains(text(), "About this journal"]/@href').inner_text
+    end
+
+    def getEditorialUri
+      # Retrieve page from about_uri
+      # Find link with text 'View full editorial board'
+      # store link as editorial_uri
+    end
+    
+    def getEditorialBoard
+
+    end
+
+end
+
+class EditorPerson
+	def initialize(name,affiliation,role)
+		@name = name
+		@affiliation = affiliation
+		@role = role
+	end
+
+	attr_reader :name, :affiliation, :role
+end
+
+ej = ElsevierJournal.new("Academic Pediatrics","http://www.sciencedirect.com/science/journal/18762859")
+ej.getAboutUri
+puts ej.about_uri
+#elsevier_master_uri = "https://www.kbplus.ac.uk/kbplus/publicExport/pkg/512?format=json"
+#elsevier_master_json = open(elsevier_master_uri)
+
+#elsevier_master = JSON.parse(File.read(elsevier_master_json))
+
+#elsevier_master["titles"].each do |t|
+#	publication_title = t["title"].chomp
+#	publication_uri = t["hostPlatformURL"].chomp
+#  ej = ElsevierJournal.new(publication_title,publication_uri)
+#  ej.getEditorialBoard
+#  ej.editorial_board.each do |p|
+#    record = {
+ #     'journal_title' => publication_title,
+#      'journal_uri' => publication_uri,
+#      'name' => p.name,
+#      'affiliation' => p.affiliation,
+#      'role' => p.role
+#    }
+#    ScraperWiki.save_sqlite(unique_keys=['journal_title','journal_uri','name','affiliation','role'],record)
+#    sleep 1
+#  end
+#end
